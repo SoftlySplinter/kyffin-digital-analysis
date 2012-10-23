ca8cd537e20fbb0acdd2057b3ff2d50339192246
Alexander Brown <alex@alexanderdbrown.com>
2012-10-23 19:10:26 +0100

    Added some functionality to read from a CSV file and perform the average on each image.

--- src/statistical.py
+++ src/statistical.py
@@ -3,6 +3,64 @@
 from optparse import OptionParser
 import cv
 import logging
+import csv
+
+DATA_DIR = 'data/'
+LOG = logging.getLogger()
+HANDLER = logging.StreamHandler()
+HANDLER.setFormatter(logging.Formatter('%(asctime)s: %(levelname)s: %(name)s: %(message)s (%(filename)s:%(lineno)s)'))
+
+
+def main():
+	'''Quick down and dirty main method.'''
+	parser = OptionParser()
+	parser.add_option("-d", "--csv", dest="csv", help="Statistically analyse CSV file", metavar="CSV")
+	parser.add_option("-l", "--loglevel", dest="loglevel", help="Set log level", default=logging.WARNING)
+
+	(options, args) = parser.parse_args()
+	
+	LOG.setLevel(options.loglevel)
+	HANDLER.setLevel(options.loglevel)
+	LOG.addHandler(HANDLER)
+
+	LOG.debug('Contents of parsed options: {0}'.format(options))
+	
+	if options.csv == None:
+		LOG.error('No Input Data specified')
+		raise ValueError('No Input Data specified')
+
+	analyse(options.csv)
+
+def analyse(data):
+	'''Analyse a CSV file which contains the data for a set of images'''
+	averages = dict()
+	with open (data, 'r') as csvFile:
+		dataReader = csv.reader(csvFile, delimiter=',', quotechar='"')
+		for row in dataReader:
+			avg = analyseRow(row)
+			if avg is None:
+				continue
+			if row[1] not in averages:
+				averages[row[1]] = [avg]
+			else:
+				averages[row[1]].append(avg)
+	for (k,v) in averages.items():
+		print '{0}: {1}'.format(k, v)
+
+def analyseRow(row):
+	'''Analyse a row in the CSV file.
+	The format of these fils is as follows:
+
+	0) Title
+	1) Year
+	2) Type
+	3) Collection
+	4) File'''
+	try:
+		image = cv.LoadImageM(DATA_DIR + row[4])
+		return AverageRGB(image)
+	except IOError:
+		return None
 
 def AverageRGB(image):
 	'''Gets the average RGB values.
@@ -16,35 +74,7 @@ def StandardDeviationRGB(image):
 	(mean, stdDev) = cv.AvgSdv(image)
 	return stdDev
 
-LOG = logging.getLogger()
-HANDLER = logging.StreamHandler()
-HANDLER.setFormatter(logging.Formatter('%(asctime)s: %(levelname)s: %(name)s: %(message)s (%(filename)s:%(lineno)s)'))
 
-def main():
-	'''Quick down and dirty main method.'''
-	parser = OptionParser()
-	parser.add_option("-i", "--infile", dest="infile", help="Statistically analyse INFILE", metavar="INFILE")
-	parser.add_option("-d", "--debug", dest="debug", action="store_true", help="Turn on debugging", default=False)
-
-	(options, args) = parser.parse_args()
-	
-	if options.debug:
-		LOG.setLevel(logging.DEBUG)
-		HANDLER.setLevel(logging.DEBUG)
-	else:
-		HANDLER.setLevel(logging.WARNING)
-
-	LOG.addHandler(HANDLER)
-
-	LOG.debug('Contents of parsed options: {0}'.format(options))
-	
-	if options.infile == None:
-		LOG.error('No Input File specified')
-		raise ValueError('No Input File specified')
-
-	image = cv.LoadImageM(options.infile)
-	LOG.debug('Average: {0}'.format(AverageRGB(image)))
-	LOG.debug('Std Dev: {0}'.format(StandardDeviationRGB(image)))
 
 if __name__ == '__main__':
 	main()
