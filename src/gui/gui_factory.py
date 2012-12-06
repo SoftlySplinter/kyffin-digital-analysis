fce5c5cb729343945123fd9fabfc977d07bb3695
Alexander Brown <alex@alexanderdbrown.com>
2012-12-06 15:12:48 +0000

    Pylinted gui_factory.py

--- src/gui/gui_factory.py
+++ src/gui/gui_factory.py
@@ -1,10 +1,15 @@
-from gui import GUI, TextGUI, GraphGUI
+'''
+Factory for GUIs.
+'''
 
-def getGUI(name='text', dataType='default'):
-	print('Using {0}-based GUI'.format(name))
-	if name == 'text':
-		return TextGUI(dataType) 
-	if name == 'graph':
-		return GraphGUI(dataType)
+from src.gui.gui import GUI, TextGUI, GraphGUI
 
-	return GUI(dataType)
+def get_gui(name='text', data_type='default'):
+    '''Get a GUI from a string.'''
+    print('Using {0}-based GUI'.format(name))
+    if name == 'text':
+        return TextGUI(data_type) 
+    if name == 'graph':
+        return GraphGUI(data_type)
+
+    return GUI(data_type)
