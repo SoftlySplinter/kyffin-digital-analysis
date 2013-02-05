f54fc470972e704782f67efc9197b93471c33b1a
Alex Brown <alex@alexanderdbrown.com>
2013-02-05 14:02:15 +0000

    Cleanup

--- kyffin/analyser.py
+++ kyffin/analyser.py
@@ -12,66 +12,66 @@ import math
 DATA_DIR = 'data/'
 
 class Analyser:
-	def __init__(self, technique, gui, ml, fiveYearBins):
-		self.technique = technique
-		self.gui = gui
-		self.ml = ml
-		self.fiveYearBins = fiveYearBins
-		
-		self.paintings = []
+    def __init__(self, technique, gui, ml, fiveYearBins):
+        self.technique = technique
+        self.gui = gui
+        self.ml = ml
+        self.fiveYearBins = fiveYearBins
+        
+        self.paintings = []
 
-	def run(self, data):
-		self.loadPaintings( data )
-		self.analyse()
+    def run(self, data):
+        self.loadPaintings( data )
+        self.analyse()
 
-		self.gui.render(self.paintings)
+        self.gui.render(self.paintings)
 
-		actual = []
-		classified = []
+        actual = []
+        classified = []
 
-		for i in range(len(self.paintings)):
-			self.classify(i, actual, classified)
+        for i in range(len(self.paintings)):
+            self.classify(i, actual, classified)
 
-		(correlation, unknown) = self.correlation(classified, actual)
-		print 'Correlation: {0} ({1})'.format(correlation, unknown)
+        (correlation, unknown) = self.correlation(classified, actual)
+        print 'Correlation: {0} ({1})'.format(correlation, unknown)
 
-		plot.figure(2)
-		plot.plot(actual, classified, 'x')
-		plot.xlabel('Actual Year')
-		plot.ylabel('Classified Year')
-		plot.show()
+        plot.figure(2)
+        plot.plot(actual, classified, 'x')
+        plot.xlabel('Actual Year')
+        plot.ylabel('Classified Year')
+        plot.show()
 
-	def classify(self, i, actual, classified):
-		toClassify = self.paintings.pop(i)
-		actualY = toClassify.year
-		self.ml.classify( toClassify, self.paintings )
+    def classify(self, i, actual, classified):
+        toClassify = self.paintings.pop(i)
+        actualY = toClassify.year
+        self.ml.classify( toClassify, self.paintings )
 
-		classifiedY = toClassify.year
+        classifiedY = toClassify.year
 
-		toClassify.year = actualY
-		self.paintings.insert(i, toClassify)
+        toClassify.year = actualY
+        self.paintings.insert(i, toClassify)
 
-		actual.append(actualY)
-		classified.append(classifiedY)
+        actual.append(actualY)
+        classified.append(classifiedY)
 
-	def correlation(self, classified, actual):
-		a = [float(x) for x in classified]
-		b = [float(x) for x in actual]
-		return stats.pearsonr(a, b)
+    def correlation(self, classified, actual):
+        a = [float(x) for x in classified]
+        b = [float(x) for x in actual]
+        return stats.pearsonr(a, b)
 
-	def analyse(self):
-		for painting in self.paintings:
-			painting.data = self.technique.analyse(painting)
+    def analyse(self):
+        for painting in self.paintings:
+            painting.data = self.technique.analyse(painting)
 
-	def loadPaintings( self, data ):
-		with open( data, 'r' ) as csvFile:
-			dataReader = csv.reader(csvFile, delimiter=',', quotechar='"')
-			for row in dataReader:
-				try:
-					painting = paint.load(row, DATA_DIR)
-					if re.match('^\d\d\d\d$', painting.year):
-						if self.fiveYearBins:
-							painting.year = math.floor(int(painting.year)/5) * 5
-						self.paintings.append(painting)
-				except IOError:
-					continue
+    def loadPaintings( self, data ):
+        with open( data, 'r' ) as csvFile:
+            dataReader = csv.reader(csvFile, delimiter=',', quotechar='"')
+            for row in dataReader:
+                try:
+                    painting = paint.load(row, DATA_DIR)
+                    if re.match('^\d\d\d\d$', painting.year):
+                        if self.fiveYearBins:
+                            painting.year = math.floor(int(painting.year)/5) * 5
+                        self.paintings.append(painting)
+                except IOError:
+                    continue
